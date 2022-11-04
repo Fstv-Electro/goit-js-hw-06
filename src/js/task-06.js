026293ff7b3164230809c20b345c9224f27619c9
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-04 09:01:28 +0200

    best solution in task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,10 +1,10 @@
 // Звикаю до написання констант на посилань
 
-const refs = {
-  input: document.querySelector("#validation-input"),
-};
+// const refs = {
+//   input: document.querySelector("#validation-input"),
+// };
 
-const dataLength = refs.input.getAttribute("data-length");
+// const dataLength = refs.input.getAttribute("data-length");
 
 // refs.input.addEventListener("blur", (event) => {
 // if (event.target.value.length == dataLength) {
@@ -16,19 +16,39 @@ const dataLength = refs.input.getAttribute("data-length");
 // }
 // });
 
-refs.input.addEventListener("blur", (event) => {
-  const valid = "valid";
-  const invalid = "invalid";
+// refs.input.addEventListener("blur", (event) => {
+//   const valid = "valid";
+//   const invalid = "invalid";
 
-  onValidInvalid(event, valid, invalid);
-});
+//   onValidInvalid(event, valid, invalid);
+// });
 
-function onValidInvalid(event, valid, invalid) {
-  if (event.target.value.length == dataLength) {
-    refs.input.classList.add(valid);
-    refs.input.classList.remove(invalid);
+// function onValidInvalid(event, valid, invalid) {
+//   if (event.target.value.length == dataLength) {
+//     refs.input.classList.add(valid);
+//     refs.input.classList.remove(invalid);
+//   } else {
+//     refs.input.classList.add(invalid);
+//     refs.input.classList.remove(valid);
+//   }
+// }
+
+const refs = {
+  input: document.querySelector("#validation-input"),
+};
+
+const dataLength = Number(refs.input.getAttribute("data-length"));
+refs.input.addEventListener("blur", onValidInvalid);
+
+function onValidInvalid(event) {
+  if (event.target.value.length === dataLength) {
+    updateClass("valid", "invalid");
   } else {
-    refs.input.classList.add(invalid);
-    refs.input.classList.remove(valid);
+    updateClass("invalid", "valid");
   }
+}
+
+function updateClass(add, rem) {
+  refs.input.classList.add(add);
+  refs.input.classList.remove(rem);
 }
